246f39e49fa77886ee77dc0fddc93dd609349f3b
Alexandr1899 <alex.sinitsin1899@gmail.com>
2022-11-27 15:45:02 +0200

    add hw

---
+++ src/Redux/store.js
@@ -0,0 +1,16 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { contactsSlice, filterSlice } from './slice'
+
+
+
+const rootReducer = combineReducers({
+    "contacts": contactsSlice.reducer,
+    "filter": filterSlice.reducer
+})
+
+
+
+export const store = configureStore({
+    reducer: rootReducer, 
+    
+})
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "Redux/operations";
+import { ContactList } from "./ContactList/ContactList";
+import { Form } from "./Form/Form";
+
+
+
 export const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {dispatch(fetchContacts())}, [dispatch])
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{marginLeft: `20px`}}>
+      <h1>Phonebook</h1>
+      <Form/>
+      <ContactList/>
     </div>
   );
 };
